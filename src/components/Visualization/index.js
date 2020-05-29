8219963f4ac1048e51f52cc3a449bf50dbaba5e7
unknown <bran.ramirez.don@gmail.com>
2020-05-29 10:06:52 -0700

    Bugfix

--- src/components/Visualization/index.js
+++ src/components/Visualization/index.js
@@ -1,32 +1,32 @@
 import React from "react";
-import Plot from "react-plotly.js";
-import { vis } from "../../data";
+// import Plot from "react-plotly.js";
+// import { vis } from "../../data";
 
 class Visualization extends React.Component {
   state = {
-    curData: [
-      {
-        type: "sunburst",
-        maxdepth: 2,
-        ids: vis.ids,
-        labels: vis.labels,
-        parents: vis.parent,
-        marker: {
-          colors: vis.labels.map((x) => {
-            return this.props.flavors.includes(x) ||
-              this.props.effects.includes(x)
-              ? "#7BAA47"
-              : "whitesmoke";
-          }),
-        },
-        textposition: "inside",
-        insidetextorientation: "radial",
-      },
-    ],
+    // curData: [
+    //   {
+    //     type: "sunburst",
+    //     maxdepth: 2,
+    //     ids: vis.ids,
+    //     labels: vis.labels,
+    //     parents: vis.parent,
+    //     marker: {
+    //       colors: vis.labels.map((x) => {
+    //         return this.props.flavors.includes(x) ||
+    //           this.props.effects.includes(x)
+    //           ? "#7BAA47"
+    //           : "whitesmoke";
+    //       }),
+    //     },
+    //     textposition: "inside",
+    //     insidetextorientation: "radial",
+    //   },
+    // ],
   };
   render() {
     return (
-      <div></div>
+      <div>visualization goes here</div>
       // <Plot data={this.state.curData} layout={{ margin: { l: 0, r: 0, b: 0, t: 0 } }} />
     );
   }
